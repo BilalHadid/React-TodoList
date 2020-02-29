d221adb762e8509ecd23b0f8822c74476d10f043
bilal hadid <hadid0648@gmail.com>
2020-03-01 01:29:30 +0500

    Updates

---
+++ precache-manifest.f69ab417a5187b7abd3da5c3d5a9e917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a398e9f3612db312f0524cc4dc73a954",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "7d188aed188956724093",
+    "url": "/React-TodoList/static/css/main.88cc7bf5.chunk.css"
+  },
+  {
+    "revision": "ff0b5aa639b0874ec0f4",
+    "url": "/React-TodoList/static/js/2.b9bbfd9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-TodoList/static/js/2.b9bbfd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d188aed188956724093",
+    "url": "/React-TodoList/static/js/main.227623a0.chunk.js"
+  },
+  {
+    "revision": "f4e374868714d5d3b0c9",
+    "url": "/React-TodoList/static/js/runtime-main.eddafacb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-TodoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
